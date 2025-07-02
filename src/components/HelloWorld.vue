9a4e52685dbeab9e42008128a1d7ea076f146f0a
chenxuepeng <hxzx.chenxuepeng@aisino.com>
2025-07-02 17:36:23 +0800

    feat: 初始化项目结构并添加基本功能
    - 创建项目基本目录结构
    - 添加 Vue 3 及 Vite 配置
    -集成 Ant Design Vue 组件库
    - 创建基本的路由和页面组件
    - 添加全局样式和图标

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <div style="height: 1000px">
+    <a-button>Add</a-button>
+  </div>
+</template>
+<script>
+import {Button} from 'ant-design-vue';
+
+const ButtonGroup = Button.Group;
+
+export default {
+  components: {
+    AButton: Button,
+    AButtonGroup: ButtonGroup,
+  },
+};
+</script>
