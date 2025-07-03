6c0f1739afd219d17743eff2f057820d237b3cdd
chenxuepeng <hxzx.chenxuepeng@aisino.com>
2025-07-03 09:49:04 +0800

    chore: 更新 .gitignore 文件并调整项目结构
    - 删除 crawl-acro/.gitignore 文件
    - 更新根目录下的 .gitignore 文件，添加新的忽略规则
    -移除 .idea/vcs.xml 中对 crawl-acro 目录的

--- crawl-acro/src/main.ts
+++ crawl-acro/src/main.ts
@@ -1,4 +1,4 @@
-import { createApp } from 'vue';
+import {createApp} from 'vue';
 import ArcoVue from '@arco-design/web-vue';
 import ArcoVueIcon from '@arco-design/web-vue/es/icon';
 import globalComponents from '@/components';
@@ -12,7 +12,6 @@ import App from './App.vue';
 // 样式通过 arco-plugin 插件导入。详见目录文件 config/plugin/arcoStyleImport.ts
 // https://arco.design/docs/designlab/use-theme-package
 import '@/assets/style/global.less';
-import './assets/style/tailwind.css';
 import '@/api/interceptor';
 
 const app = createApp(App);
